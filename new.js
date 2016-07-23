af03e465356c6cc7a70b27601b349625c157a930
Theo Moule <theomoule@Theos-MacBook-Pro.local>
2016-07-23 14:39:03 +1000

    adding first test file

---
+++ new.js
@@ -0,0 +1,45 @@
+if (a == 1) {
+	expression
+};
+
+var fifty = 0;
+while (fifty < 50) 
+{
+	console.log(fifty);
+	fifty = fifty + 2
+}
+
+
+var index = 0
+while (index < 50){
+console.log(index);
+index = index + 2;
+}
+
+var dom = 100
+while (dom > 50){
+console.log(dom);
+dom = dom - 2;
+}
+
+
+
+
+var index = 0
+while (index < 50){
+
+var dom = 100
+while (dom > 50){
+console.log(dom);
+dom = dom - 2;
+}
+console.log(index);
+index = index + 2;
+}
+
+
+var dom = 100
+while (dom > 50){
+console.log(dom);
+dom = dom - 2;
+}
